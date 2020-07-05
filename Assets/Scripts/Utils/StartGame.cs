b8bfb4156df0576bd037225b05f49a7cfaa4acb8
antoidco <antoidco@gmail.com>
2020-07-05 20:57:45 +0300

    strange build fix

--- Assets/Scripts/Utils/StartGame.cs
+++ Assets/Scripts/Utils/StartGame.cs
@@ -5,15 +5,20 @@ using UnityEngine.SceneManagement;
 namespace Utils {
     public class StartGame : MonoBehaviour {
         public static bool withBot = false;
+        public MenuStartup menuStartup;
 
         private void OnTriggerEnter(Collider other) {
             if (other.tag == "Game1v1") {
                 withBot = false;
+                Destroy(menuStartup);
+                menuStartup = null;
                 SceneManager.LoadScene("Main");
             }
 
             if (other.tag == "GameWithBot") {
                 withBot = true;
+                Destroy(menuStartup);
+                menuStartup = null;
                 SceneManager.LoadScene("Main");
             }
         }
